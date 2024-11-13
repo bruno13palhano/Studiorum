259788cc66e0471cd163e008de3d988e4fc2f9fe
bruno13palhano <bruno13palhano@gmail.com>
2024-11-13 20:24:56 -0300

    Implement custom MotionFrameClock

--- composeApp/src/commonMain/kotlin/com/bruno13palhano/ui/shared/MainClock.kt
+++ composeApp/src/commonMain/kotlin/com/bruno13palhano/ui/shared/MainClock.kt
@@ -1,4 +1,9 @@
 package com.bruno13palhano.ui.shared
 
-class MainClock {
+import androidx.compose.runtime.MonotonicFrameClock
+
+class MainClock : MonotonicFrameClock {
+    override suspend fun <R> withFrameNanos(onFrame: (frameTimeNanos: Long) -> R): R {
+        return onFrame(System.nanoTime())
+    }
 }
