cb00f5bf21ec86ccaf57adea54623478cf9f38b6
bruno13palhano <bruno13palhano@gmail.com>
2024-11-19 22:16:16 -0300

    Add BooksViewModel, NewBookViewModel and EditBookViewModel to viewModelModule

--- composeApp/src/commonMain/kotlin/com/bruno13palhano/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/bruno13palhano/di/ViewModelModule.kt
@@ -1,9 +1,15 @@
 package com.bruno13palhano.di
 
+import com.bruno13palhano.ui.books.books.viewmodel.BooksViewModel
+import com.bruno13palhano.ui.books.editbook.viewmodel.EditBookViewModel
+import com.bruno13palhano.ui.books.newbook.viewmodel.NewBookViewModel
 import com.bruno13palhano.ui.home.viewmodel.HomeViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::HomeViewModel)
+    viewModelOf(::BooksViewModel)
+    viewModelOf(::NewBookViewModel)
+    viewModelOf(::EditBookViewModel)
 }
